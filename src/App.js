67baa1e15147203c3944f0c1b2beb6267147a88d
cclarev <cclarev@est.emi.edu.bo>
2022-04-10 22:13:18 -0400

    Navegacion y barra de navegacion agregados

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import Profile from './pages/Profile';
-import Shop from './pages/Shop';
-import Register from './pages/Register';
-import Login from './pages/Login';
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Profile from "./pages/Profile";
+import Shop from "./pages/Shop";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/shop' element={<Shop />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/register' element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,44 @@
-export default function() {
-    return (
-        <div>
-            <h1>Navbar</h1>
+import { NavLink } from "react-router-dom";
+
+export default function () {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <h1 className="navbar-brand">Aquacorp</h1>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/login" className="nav-link">
+              Login
+            </NavLink>
+            <NavLink to="/shop" className="nav-link">
+              Shop
+            </NavLink>
+            <NavLink to="/cart" className="nav-link">
+              Cart
+            </NavLink>
+            <NavLink to="/profile" className="nav-link">
+              Profile
+            </NavLink>
+            <NavLink to="/register" className="nav-link">
+              Register
+            </NavLink>
+          </div>
         </div>
-    );
+      </div>
+    </nav>
+  );
 }
