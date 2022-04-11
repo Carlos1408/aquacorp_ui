ccc713222704d66cca9ca299ecf1039f7eb08a27
cclarev <cclarev@est.emi.edu.bo>
2022-04-10 20:09:26 -0400

    Rutas generales creadas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './components/home/Home';
+import Cart from './components/cart/Cart';
+import Profile from './components/profile/Profile';
+import Shop from './components/shop/Shop';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/register/Register.js
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+        <div>
+            <h1>Register</h1>
+        </div>
+    );
+}
