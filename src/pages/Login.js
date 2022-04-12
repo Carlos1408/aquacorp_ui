14287120fe1c5cbe77219e97e017a1b6865c6eb3
cclarev <cclarev@est.emi.edu.bo>
2022-04-11 22:23:50 -0400

    Ui de profile y login creados

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,44 @@
+import logo from "../assets/logo.png";
+
 export default function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
+  return (
+    <div className="pt-5">
+      <div className="text-center d-flex justify-content-center align-items-center">
+        <div className="form-signin">
+          <form>
+            <img className="mb-4" src={logo} alt="" width="100" height="100" />
+            <h1 className="h3 mb-3 fw-normal">Inicio de sesion</h1>
+
+            <div className="form-floating my-3">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput">Correo electronico</label>
+            </div>
+            <div className="form-floating my-3">
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Contraseña"
+              />
+              <label for="floatingPassword">Contraseña</label>
+            </div>
+
+            <div className="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me" /> Recuerdame
+              </label>
+            </div>
+            <button className="w-100 btn btn-lg btn-primary" type="submit">
+              Iniciar sesion
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,57 @@
+import user from "../data/user.json";
+import photo from "../assets/photo.png";
+
 export default function Profile() {
-    return (
-        <div>
-            <h1>Profile</h1>
+  return (
+    <div className="pt-4">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4">
+            <img
+              src={photo}
+              className="rounded-circle mx-auto d-block"
+              width="200"
+            />
+            <button className="btn btn-info btn-lg my-4 mx-auto d-block">
+                Editar datos
+            </button>
+            
+          </div>
+          <div className="col-lg-8">
+            <div className="card bg-light">
+              <div className="card-title">
+                <h2 className="m-3">Informacion personal</h2>
+              </div>
+              <div className="card-body">
+                <div className="mb-3">
+                  <label for="name" className="form-label">
+                    Nombre
+                  </label>
+                  <input type="text" className="form-control" id="name" value={user.name} readOnly/>
+                </div>
+                <div className="mb-3">
+                  <label for="lastname" className="form-label">
+                    Apellido
+                  </label>
+                  <input type="text" className="form-control" id="lastname" value={user.lastname} readOnly/>
+                </div>
+                <div className="mb-3">
+                  <label for="birthdate" className="form-label">
+                    Fecha de nacimiento
+                  </label>
+                  <input type="date" className="form-control" id="birthdate" />
+                </div>
+                <div className="mb-3">
+                  <label for="lastname" className="form-label">
+                    Correo electronico
+                  </label>
+                  <input type="text" className="form-control" id="email" value={user.email} readOnly/>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
